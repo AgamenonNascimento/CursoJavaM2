35f7b424c4e36e0d7f084bc4c314af1684950a99
agamenon <agamenonnas@gmail.com>
2025-12-22 08:51:55 -0300

    primeiro commit

---
+++ src/com/agamenon/cursojavam2/aula53/Teste.java
@@ -0,0 +1,9 @@
+package com.agamenon.cursojavam2.aula53;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello word");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module CursoJavaM2 {
+}
