a9927f9efe902747253ef01d99c8d80766a7d6e9
agamenon <agamenonnas@gmail.com>
2025-12-23 07:51:56 -0300

    commit class Teste aula54

---
+++ src/com/agamenon/cursojavam2/aula54/Teste.java
@@ -0,0 +1,17 @@
+package com.agamenon.cursojavam2.aula54;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		DiaSemana dia = DiaSemana.SEGUNDA;
+		
+		System.out.println(dia.toString() + " - " + dia.getValor());
+		
+		Data data = new Data( 1, 12, 2025 , DiaSemana.SEGUNDA);
+		
+		System.out.println(data.getDiasemana());
+
+	}
+
+}
