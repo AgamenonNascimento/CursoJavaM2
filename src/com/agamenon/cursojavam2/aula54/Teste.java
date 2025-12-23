0b6884efcd009359e12d4302322c9010d7eb7eb8
agamenon <agamenonnas@gmail.com>
2025-12-23 11:46:22 -0300

    commit sincronizacao

--- src/com/agamenon/cursojavam2/aula54/Teste.java
+++ src/com/agamenon/cursojavam2/aula54/Teste.java
@@ -3,18 +3,15 @@ package com.agamenon.cursojavam2.aula54;
 public class Teste {
 
 	public static void main(String[] args) {
-		
+
 		DiaSemana dia = DiaSemana.SEGUNDA;
-		
+
 		System.out.println(dia.toString() + " - " + dia.getValor());
-		
-		Data data = new Data(3, 12, 2025 , DiaSemana.SEGUNDA);
-		
+
+		Data data = new Data(4, 12, 2025, DiaSemana.SEGUNDA);
+
 		System.out.println(data.getDiasemana());
-		
-		
-		
-		
+
 	}
 
 }
