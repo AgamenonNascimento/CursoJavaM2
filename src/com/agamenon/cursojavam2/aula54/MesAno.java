2250268697f3a5648d34c7862b0f35aa1fac0559
agamenon <agamenonnas@gmail.com>
2025-12-23 12:39:24 -0300

    commit enum MesAno

---
+++ src/com/agamenon/cursojavam2/aula54/MesAno.java
@@ -0,0 +1,40 @@
+package com.agamenon.cursojavam2.aula54;
+
+public enum MesAno {
+  JANEIRO("Janeiro",1),
+  FEVEREIRO("Fevereiro",2),   
+  MARCO("Março", 3),
+  ABRIL("Abril", 3),
+  MAIO("Maio", 3),
+  JUNHO("Junho", 3),
+  JULHO("Julho", 3),
+  AGOSTO("Agosto", 3),
+  SETEMBRO("Setembro", 3),
+  OUTUBRO("Outubro", 3),
+  NOVEMBRO("Novembro", 3),
+  DEZEMBRO("Dezembro", 3);
+
+  private String nome;
+  private int valor;
+  
+  private MesAno() {
+	
+}
+
+  MesAno(String string, int valor) {
+	  this.nome = nome;
+	  this.valor = valor;
+  }
+
+  public int getValor() {
+    return valor;
+  }
+
+  public String getNome() {
+	return nome;
+  }
+
+  
+  
+  
+}
