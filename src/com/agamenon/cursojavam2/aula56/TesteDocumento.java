54cf47fb18a06661eec2c21a8d2028564ef03152
agamenon <agamenonnas@gmail.com>
2025-12-26 05:41:37 -0300

    class TesteDocumento

---
+++ src/com/agamenon/cursojavam2/aula56/TesteDocumento.java
@@ -0,0 +1,24 @@
+package com.agamenon.cursojavam2.aula56;
+
+public class TesteDocumento {
+
+	public static void main(String[] args) {
+		/*for(TipoDocumento doc : TipoDocumento.values()) {
+			System.out.println(doc + " - " + doc.geraNumeroTeste());
+		}*/
+		
+		Pessoa pf = new Pessoa();
+		pf.setTipoDocumento(Enum.valueOf(TipoDocumento.class, "CPF"));
+		pf.setNumeroDocumento(pf.getTipoDocumento().geraNumeroTeste());
+		System.out.println(pf);
+		
+		System.out.println("Pessoa Juridica:");
+		
+		Pessoa pj = new Pessoa();
+		pj.setTipoDocumento(Enum.valueOf(TipoDocumento.class, "CNPJ"));
+		pj.setNumeroDocumento(pj.getTipoDocumento().geraNumeroTeste());
+		System.out.println(pj);
+
+	}
+
+}
